18ad48f9a8d6d37fbf4261c89000ce72ebe96c8e
studiolhb <csstudiolhb@gmail.com>
2026-04-15 23:27:18 -0700

    Sort designs array by name
    Added sorting functionality to designs array.

--- designs.js
+++ designs.js
@@ -3,6 +3,8 @@
 // Categories: abstract, animal, blender, botanical, coastal, fall, food-kitchen, geometric, holiday, night-sky, origami, rainbow, recreation, spring, summer, vintage-retro, winter, baby-kids
 //
 const designs = [
+  const sorted = [...designs].sort((a, b) => a.name.localeCompare(b.name));
+sorted.forEach(d => {
   {
     name: "Back to School ABC 123 Apples Books Teacher Classroom",
     img: "Back to School ABC 123 Apples Books Teacher Classroom.jpeg",
@@ -818,6 +820,7 @@ const designs = [
     cats: ["summer", "botanical"],
     status: "live",
     url: "https://www.spoonflower.com/en/artists/fabric/22008841"
-  },
+   },
+               });
 ];
 
